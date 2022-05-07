83fb57d7070d8f08488e9037269b7b5893d755bf
omnireader0 <93963499+omnireader0@users.noreply.github.com>
2022-05-07 21:32:50 +0900

    Update readme.py

--- readme.py
+++ readme.py
@@ -5,6 +5,7 @@ feed = feedparser.parse(blog_rss_uri)
 
 markdown_text = """
 
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fomnireader0&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
 ### ✍ Recent blog posts 
 """ # list of blog posts will be appended here
 
